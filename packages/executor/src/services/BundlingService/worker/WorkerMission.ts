132daabf169866068257d2f50b640bc007336926
Ghoulouis <maikhanh3241@gmail.com>
2024-05-30 17:53:18 +0700

    hotfix: worker

--- packages/executor/src/services/BundlingService/worker/WorkerMission.ts
+++ packages/executor/src/services/BundlingService/worker/WorkerMission.ts
@@ -13,107 +13,106 @@ const bundleGasLimitMarkup = 25000;
 const provider = new providers.JsonRpcProvider("https://testnet.sapphire.oasis.io");
 
 export async function log(text: string) {
-  parentPort!.postMessage({ log: "✅" + text });
+    parentPort!.postMessage({ log: "✅" + text });
 }
 
 export async function logError(text: string) {
-  parentPort!.postMessage({ log: "❌" + text });
+    parentPort!.postMessage({ log: "❌" + text });
 }
 
 export async function updateStatus(
-  entries: MempoolEntry[],
-  status: MempoolEntryStatus,
-  params?: {
-    transaction?: string;
-    revertReason?: string;
-  }
+    entries: MempoolEntry[],
+    status: MempoolEntryStatus,
+    params?: {
+        transaction?: string;
+        revertReason?: string;
+    }
 ) {
-  parentPort!.postMessage({
-    updateStatus: {
-      entries,
-      status,
-      params,
-    },
-  });
+    parentPort!.postMessage({
+        updateStatus: {
+            entries,
+            status,
+            params,
+        },
+    });
 }
 
 parentPort!.on("message", async () => {
-  let result = await asyncFunction(workerData);
-  parentPort!.postMessage({ result });
+    let result = await asyncFunction(workerData);
+    log("Worker finished");
+    parentPort!.postMessage({ result: result });
 });
 
 async function asyncFunction(data: any): Promise<any> {
-  try {
-    log("Worker started");
-    const { entries, privateKey } = data;
+    try {
+        log("Worker started");
+        const { entries, privateKey } = data;
 
-    if (!entries.length) {
-      log("No new entries");
-      return;
-    }
+        if (!entries.length) {
+            log("No new entries");
+            return;
+        }
 
-    const relayer = new ethers.Wallet(privateKey, provider);
+        const relayer = new ethers.Wallet(privateKey, provider);
 
-    const gasFee = await getGasFee(chainId, provider, "");
+        const gasFee = await getGasFee(chainId, provider, "");
 
-    if (gasFee.gasPrice == undefined && gasFee.maxFeePerGas == undefined && gasFee.maxPriorityFeePerGas == undefined) {
-      return;
-    }
-    log(" create Bundler");
-    const bundle = await createBundle(gasFee, entries, provider);
+        if (gasFee.gasPrice == undefined && gasFee.maxFeePerGas == undefined && gasFee.maxPriorityFeePerGas == undefined) {
+            return;
+        }
+        log(" create Bundler");
+        const bundle = await createBundle(gasFee, entries, provider);
 
-    //await mempoolService.updateStatus(bundle.entries, MempoolEntryStatus.Pending);
+        //await mempoolService.updateStatus(bundle.entries, MempoolEntryStatus.Pending);
 
-    //await mempoolService.attemptToBundle(bundle.entries);
+        //await mempoolService.attemptToBundle(bundle.entries);
 
-    //relayer send transaction
+        //relayer send transaction
 
-    const { entries: bundleEntries } = bundle;
+        const { entries: bundleEntries } = bundle;
 
-    const wallet = new ethers.Wallet(privateKey, provider);
+        const wallet = new ethers.Wallet(privateKey, provider);
 
-    const beneficiary = await wallet.getAddress();
+        const beneficiary = await wallet.getAddress();
 
-    const entryPoint = bundleEntries[0]!.entryPoint;
+        const entryPoint = bundleEntries[0]!.entryPoint;
 
-    const entryPointContract = IEntryPoint__factory.connect(entryPoint, provider);
+        const entryPointContract = IEntryPoint__factory.connect(entryPoint, provider);
 
-    const txRequest = entryPointContract.interface.encodeFunctionData("handleOps", [bundleEntries.map((entry) => entry.userOp), beneficiary]);
+        const txRequest = entryPointContract.interface.encodeFunctionData("handleOps", [bundleEntries.map((entry) => entry.userOp), beneficiary]);
 
-    const transactionRequest: providers.TransactionRequest = {
-      to: entryPoint,
-      data: txRequest,
-      gasPrice: 100e9,
-    };
-
-    const transaction = {
-      ...transactionRequest,
-      gasLimit: estimateBundleGasLimit(bundleGasLimitMarkup, bundle.entries),
-      chainId: provider._network.chainId,
-      nonce: await relayer.getTransactionCount(),
-    };
-
-    await submitTransaction(relayer, transaction)
-      .then(async (hash) => {
-        log("Transaction hash: " + hash);
-        // setSubmitted(bundle.entries, hash);
-        return {
-          success: true,
-          hash: hash,
+        const transactionRequest: providers.TransactionRequest = {
+            to: entryPoint,
+            data: txRequest,
+            gasPrice: 100e9,
         };
-      })
-      .catch(async (error) => {
-        logError("Transaction error: " + error);
-        // setFailed(bundle.entries, error);
-        return {
-          success: false,
-          error: error,
+
+        const transaction = {
+            ...transactionRequest,
+            gasLimit: estimateBundleGasLimit(bundleGasLimitMarkup, bundle.entries),
+            chainId: provider._network.chainId,
+            nonce: await relayer.getTransactionCount(),
         };
-      });
-  } catch (error) {
-    return {
-      success: false,
-      error: error,
-    };
-  }
+
+        return await submitTransaction(relayer, transaction)
+            .then(async (hash) => {
+                log("Transaction hash: " + hash);
+                return {
+                    success: true,
+                    hash: hash,
+                };
+            })
+            .catch(async (error) => {
+                logError("Transaction error: " + error);
+                return {
+                    success: false,
+                    error: error,
+                };
+            });
+    } catch (error) {
+        return {
+            success: false,
+            error: error,
+        };
+    }
 }
--- packages/executor/src/services/BundlingService/worker/worker.ts
+++ packages/executor/src/services/BundlingService/worker/worker.ts
@@ -5,49 +5,49 @@ import { MempoolEntryStatus } from "types/lib/executor";
 import { IRelayingMode } from "../interfaces";
 
 export function runService(workerData: any, logger: Logger, mempoolService: MempoolService, idRelayer: number, relayer: IRelayingMode) {
-  return new Promise((resolve, reject) => {
-    const worker = new wk.Worker("./packages/executor/lib/services/BundlingService/worker/WorkerMission.js", { workerData });
-    worker.on("message", async (message) => {
-      // send log from child thread
-      if (message.log) {
-        logger.debug(` 😵 Worker log:${message.log}`);
-      }
-      //action update status
-      if (message.updateStatus) {
-        const { entries, status, params } = message.updateStatus;
-        await mempoolService.updateStatus(entries, status, params);
-      }
-      // result thread
-      if (message.result) {
-        const { success } = message.result;
-        if (success) {
-          // send bundler success
-          const { hash } = message.result;
-          await mempoolService.updateStatus(workerData.entries, MempoolEntryStatus.Finalized);
-          await relayer.setSubmitted(workerData.entries, hash);
-        } else {
-          // send bundler failed
-          const { error } = message.result;
-          await relayer.handleUserOpFail(workerData.entries, error);
-        }
-        relayer.unlockRelayer(idRelayer);
-        resolve(message.result);
-      }
-    });
+    return new Promise((resolve, reject) => {
+        const worker = new wk.Worker("./packages/executor/lib/services/BundlingService/worker/WorkerMission.js", { workerData });
+        worker.on("message", async (message) => {
+            // send log from child thread
+            if (message.log) {
+                logger.debug(` 😵 Worker log:${message.log}`);
+            }
+            //action update status
+            if (message.updateStatus) {
+                const { entries, status, params } = message.updateStatus;
+                await mempoolService.updateStatus(entries, status, params);
+            }
+            // result thread
+            if (message.result) {
+                const { success } = message.result;
+                if (success) {
+                    // send bundler success
+                    const { hash } = message.result;
+                    await mempoolService.updateStatus(workerData.entries, MempoolEntryStatus.Finalized);
+                    await relayer.setSubmitted(workerData.entries, hash);
+                } else {
+                    // send bundler failed
+                    const { error } = message.result;
+                    await relayer.handleUserOpFail(workerData.entries, error);
+                }
+                relayer.unlockRelayer(idRelayer);
+                resolve(message.result);
+            }
+        });
 
-    worker.on("error", () => {
-      relayer.unlockRelayer(idRelayer);
-      reject;
-    });
+        worker.on("error", () => {
+            relayer.unlockRelayer(idRelayer);
+            reject;
+        });
 
-    worker.on("exit", (code) => {
-      relayer.unlockRelayer(idRelayer);
-      if (code !== 0) {
-        relayer.unlockRelayer(idRelayer);
-        reject(new Error(`Worker stopped with exit code ${code}`));
-      }
-    });
+        worker.on("exit", (code) => {
+            relayer.unlockRelayer(idRelayer);
+            if (code !== 0) {
+                relayer.unlockRelayer(idRelayer);
+                reject(new Error(`Worker stopped with exit code ${code}`));
+            }
+        });
 
-    worker.postMessage(workerData);
-  });
+        worker.postMessage(workerData);
+    });
 }
